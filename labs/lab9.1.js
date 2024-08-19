962635d19c4e73f36784702a3a1617c1e2a7ed40
nhanlam <lamnhan106997@gmail.com>
2024-08-19 22:20:42 +0700

    lab9.1 update

--- labs/lab9.1.js
+++ labs/lab9.1.js
@@ -1,35 +1,34 @@
-const url = 'https://jsonplaceholder.typicode.com/posts'
-// console.log(post);
-const userId = 1;
-const postId = 1;
-printTargetPost(userId, postId)
-printAllPosts(userId)
+const url = 'https://jsonplaceholder.typicode.com/posts';
+const user_id = 1;
+const post_id = 1;
 
-function printTargetPost(userId, postId){
-    getPostByUserId(userId).then(function (posts){
-        const targetPost = posts.filter(function(post){
-            return post.id === postId
+print_target_post(user_id, post_id);
+print_all_posts(user_id);
+
+function print_target_post(user_id, post_id) {
+    get_posts_by_user_id(user_id).then(function(posts) {
+        const target_post = posts.filter(function(post) {
+            return post.id === post_id;
         });
-        console.log(targetPost[0]);
-        
-    })
+        console.log(target_post[0]);
+    });
 }
 
-function printAllPosts(userId){
-    getPostByUserId(userId).then(function (posts){
+function print_all_posts(user_id) {
+    get_posts_by_user_id(user_id).then(function(posts) {
         console.log(posts);
-        
-    })
+    });
 }
-function getPostByUserId(userId) {
-    return send_request(url).then(function (posts) {
-        return posts.filter(function (post) {
-            return post.userId === userId
-        })
-    })
+
+function get_posts_by_user_id(user_id) {
+    return send_request(url).then(function(posts) {
+        return posts.filter(function(post) {
+            return post.userId === user_id;
+        });
+    });
 }
 
 function send_request(url) {
     return fetch(url)
-        .then(response => response.json())
+        .then(response => response.json());
 }
